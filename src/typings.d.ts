eebf14b52e2483da817e7af8e53c1d4a1fbaaeb2
Kevin <wcdma020@hotmail.com>
2024-05-10 00:40:13 +0800

    chore: 新增及调整一些参数类型

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -8,14 +8,30 @@
  * +----------------------------------------------------------------------------------
  */
 
+import { SortOrder } from 'antd/es/table/interface'
+import { ParamsType } from '@ant-design/pro-components'
+
 declare namespace API {
     interface LoginResponse {
         uid: number
         usename: string
-        accessToken: string
-        refreshToken: string
-        accessTokenExpAt: string
-        refreshTokenExpAt: string
+        access_token: string
+        refresh_token: string
+    }
+
+    interface JwtPayloadProp {
+        ip: string
+        iss: string
+        sub: string
+        uid: number
+        exp: number
+        nbf: number
+        iat: number
+        jti: string
+        aud: string[]
+        username: string
+        tokenType: string
+        userAgent: string
     }
 
     interface BasicResponseSchema {
@@ -28,4 +44,8 @@ declare namespace API {
         info?: Record<string, any>
         list?: Record<string, any>
     }
+
+    export type ProTableSort = Record<string, SortOrder>
+
+    export type ProTableParams = ParamsType & { pageSize?: number | undefined; current?: number | undefined; keyword?: string | undefined; }
 }
